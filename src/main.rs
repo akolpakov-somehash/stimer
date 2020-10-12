bf2ab6c96342d8de31a3dc9ae6f474020e399bfa
Alex K <akolpakov@tuble.pw>
2020-10-12 23:04:25 +0300

    subcommands

--- src/main.rs
+++ src/main.rs
@@ -3,49 +3,36 @@ use std::process;
 use std::error::Error;
 use structopt::StructOpt;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem with parsing arguments: {}", err);
-        process::exit(1);
-    }
-    );
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
-        process::exit(1);
-    }
-}
-
-#[derive(StructOpt)]
-struct Cli {
-    cmd: String,
-    record: String,
-}
-
-struct Config {
-    cmd: String,
-    record: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    match ST::from_args() {
+        ST::Start {record} => {
+            println!("Start {}", record)
         }
-        if args.len() >3 {
-            return Err("too much arguments");
+        ST::Stop {} => {
+            println!("Stop")
         }
-        let cmd = args[1].clone();
-        let record = args[2].clone();
-
-        Ok(Config {cmd, record})    
+        ST::Report {start, end} => {
+            println!("Start {}, end {}", start, end)
+        }
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("CMD {}", config.cmd);
-    println!("record {}", config.record);
-
     Ok(())
 }
+
+
+#[derive(StructOpt)]
+#[structopt(about = "simple timer")]
+enum ST {
+    Start {
+        record: String
+    },
+    Stop {
+
+    },
+    Report {
+        #[structopt(short)]
+        start: String,
+        #[structopt(short)]
+        end: String
+    }
+}
+
